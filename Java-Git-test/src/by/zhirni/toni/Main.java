7925973534fabbb203b87620525c56ecb000ab96
amans <amans@AmansComputer>
2016-12-05 01:03:36 +0600

    added 1

---
+++ Java-Git-test/src/by/zhirni/toni/Main.java
@@ -0,0 +1,9 @@
+package by.zhirni.toni;
+
+public class Main {
+	public static void main(String args[]){
+		System.out.println("Bagdat is one of the stupid students of IITU");
+		System.out.println("Amandik Yermaganbetov");
+		
+	}
+}

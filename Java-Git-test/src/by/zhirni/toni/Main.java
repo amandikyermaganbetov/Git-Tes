75ffda723160a3894b8da45a21671c7900b17c6c
amans <amans@AmansComputer>
2016-12-05 09:48:58 +0600

    latest

--- Java-Git-test/src/by/zhirni/toni/Main.java
+++ Java-Git-test/src/by/zhirni/toni/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String args[]){
 		System.out.println("Bagdat is one of the stupid students of IITU");
 		System.out.println("Amandik Yermaganbetov");
+		System.out.println("Yermaganbetov asdasda");
+		
 		
 	}
 }
